6223a851a1414f7f6af0d50b52870d2c7f93520e
Jeffrey Ma <18jeffreyma@gmail.com>
2020-08-19 18:03:41 -0700

    updated script

--- example_gen_e2e_example/run.sh
+++ example_gen_e2e_example/run.sh
@@ -6,10 +6,10 @@ screen -S "Simulate Stream"
 screen -S "Materialize"
 
 echo "Starting beam streaming materialization listener."
-screen -S "Materialize" -X "python3 materialize_stream.py <CMD>\n"
+screen -S "Materialize" -X "python3 materialize_stream.py"
 
 echo "Starting taxi data stream simulator."
-screen -d -m bash -S 'Simulate Stream' -X "python3 simulate_taxi_data_stream.py <CMD>\n"
+screen -S 'Simulate Stream' -X "python3 simulate_taxi_data_stream.py"
 
 echo "Opening monitor screen."
 screen -S 'Monitor'
