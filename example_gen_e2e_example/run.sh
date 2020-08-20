d0b32208edccc1b686e30d29c85df7009c47a2fb
Jeffrey Ma <18jeffreyma@gmail.com>
2020-08-19 18:28:35 -0700

    added

--- example_gen_e2e_example/run.sh
+++ example_gen_e2e_example/run.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 echo "Creating screens."
-screen -S "Monitor"
 screen -S "Simulate Stream"
+
 screen -S "Materialize"
 
+
 echo "Starting beam streaming materialization listener."
-screen -S "Materialize" -X "python3 materialize_stream.py"
+screen -S "Materialize" -X stuff 'python3 materialize_stream.py <CMD>\n'
 
 echo "Starting taxi data stream simulator."
-screen -S 'Simulate Stream' -X "python3 simulate_taxi_data_stream.py"
+screen -S 'Simulate Stream' -X stuff 'python3 simulate_taxi_data_stream.py <CMD>\n'
 
-echo "Opening monitor screen."
+echo "Creating monitor screen."
 screen -S 'Monitor'
